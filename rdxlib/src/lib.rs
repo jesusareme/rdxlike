c732de891631a8441de76c6b58c482899ab11e52
Jesús A. <j.arenas@me.com>
2026-08-11 09:22:04 +0200

    First iteration on runtime testing done

--- rdxlib/src/lib.rs
+++ rdxlib/src/lib.rs
@@ -172,16 +172,16 @@ mod tests {
     use std::assert_matches;
     use std::cell::{Cell, RefCell};
     use std::rc::Rc;
-    use std::sync::mpsc;
     use std::sync::mpsc::TryRecvError;
+    use std::sync::{Arc, RwLock, mpsc};
 
-    struct MClient;
-    impl Client for MClient {
-        type State = BasicState;
-        type Action = FakeAction;
-        type RuntimeAction = BasicRuntimeAction;
-        type Flag = BasicFlag;
-        type ServiceCommand = FakeServiceCommand;
+    struct TestClient;
+    impl Client for TestClient {
+        type State = WitnessState;
+        type Action = TestAction;
+        type RuntimeAction = TestRuntimeAction;
+        type Flag = TestFlag;
+        type ServiceCommand = TestServiceCommand;
     }
 
     struct WitnessServices {
@@ -189,41 +189,46 @@ mod tests {
     }
 
     #[derive(Debug, PartialEq, Clone)]
-    struct BasicState {
-        called: Vec<FakeAction>,
+    struct WitnessState {
+        called: Vec<TestAction>,
     }
 
     #[derive(Debug, PartialEq, Clone)]
     enum CmdProduct {
-        Direct(Vec<FakeAction>),
-        Queue(Vec<FakeAction>),
-        Async(Box<FakeAction>),
-        Env(FakeServiceCommand),
+        Direct(Vec<TestAction>),
+        Queue(Vec<TestAction>),
+        Async(Box<TestAction>),
+        Env(TestServiceCommand),
     }
 
     #[derive(Debug, PartialEq, Clone)]
-    enum FakeAction {
+    enum TestAction {
         BasicAction(&'static str),
-        FlagAction(BasicFlag),
+        FlagAction(TestFlag),
         CmdGeneratingAction(&'static str, CmdProduct),
     }
 
-    impl Into<Message<FakeAction, BasicRuntimeAction>> for FakeAction {
-        fn into(self) -> Message<FakeAction, BasicRuntimeAction> {
-            Message::Action(self)
+    impl Into<Message<TestAction, TestRuntimeAction>> for TestAction {
+        fn into(self) -> Message<TestAction, TestRuntimeAction> {
+            Action(self)
         }
     }
 
     #[derive(Debug, PartialEq)]
-    struct BasicRuntimeAction;
-    impl Into<Message<FakeAction, BasicRuntimeAction>> for BasicRuntimeAction {
-        fn into(self) -> Message<FakeAction, BasicRuntimeAction> {
+    enum TestRuntimeAction {
+        CreateSubscriber(WitnessSubscriber),
+        CreateActions(Vec<TestAction>),
+    }
+
+
+    impl Into<Message<TestAction, TestRuntimeAction>> for TestRuntimeAction {
+        fn into(self) -> Message<TestAction, TestRuntimeAction> {
             Message::Runtime(self)
         }
     }
 
     #[derive(EnumSetType, Debug)]
-    enum BasicFlag {
+    enum TestFlag {
         A,
         B,
         C,
@@ -232,63 +237,72 @@ mod tests {
     }
 
     #[derive(Debug, PartialEq, Clone)]
-    struct FakeServiceCommand;
-    impl EnvironmentCommand for FakeServiceCommand {
+    struct TestServiceCommand;
+    impl EnvironmentCommand for TestServiceCommand {
         type Environment = WitnessServices;
         fn process(self, env: &mut Self::Environment) {
             env.called.update(|c| c + 1)
         }
     }
 
-    struct BasicJobDispatcher {
+    struct WitnessJobDispatcher {
         pub called: Rc<Cell<u32>>,
     }
 
-    impl JobsDispatcher for BasicJobDispatcher {
+    impl JobsDispatcher for WitnessJobDispatcher {
         fn work_on(&self, job: Box<dyn FnOnce() + Send + 'static>) {
             self.called.update(|v| v + 1);
             job()
         }
     }
 
-    type MessageWitness = Rc<RefCell<Vec<FakeAction>>>;
+    type MessageWitness = Rc<RefCell<Vec<TestAction>>>;
     struct WitnessMiddleware {
         messages: MessageWitness,
     }
-    impl ChainableMiddleware<MClient> for WitnessMiddleware {
+    impl ChainableMiddleware<TestClient> for WitnessMiddleware {
         fn execute(
             &mut self,
-            state: &mut BasicState,
-            action: FakeAction,
-            mut next: Next<MClient>,
-        ) -> ActionProducts<MClient> {
+            state: &mut WitnessState,
+            action: TestAction,
+            mut next: Next<TestClient>,
+        ) -> ActionProducts<TestClient> {
             self.messages.borrow_mut().push(action.clone());
             next.run(state, action)
         }
     }
 
+    #[derive(Debug)]
     struct WitnessSubscriber {
-        operations: Rc<RefCell<Vec<WitnessSubscriberChecks>>>,
+        name: &'static str,
+        operations: Arc<RwLock<Vec<WitnessSubscriberChecks>>>,
         should_be_interested: bool,
         should_be_active: bool,
         should_notify_error: bool,
     }
 
+    impl PartialEq for WitnessSubscriber {
+        fn eq(&self, other: &Self) -> bool {
+            self.name.eq(other.name)
+        }
+    }
+
     #[derive(PartialEq, Debug, Clone)]
     enum WitnessSubscriberChecks {
-        Interested(EnumSet<BasicFlag>),
-        Notify(BasicState),
+        Interested(EnumSet<TestFlag>),
+        Notify(WitnessState),
         Active,
     }
     use crate::cmd::AsyncTask;
     use crate::messages::Message::Action;
-    use crate::tests::FakeAction::{BasicAction, CmdGeneratingAction, FlagAction};
+    use crate::tests::TestAction::{BasicAction, CmdGeneratingAction, FlagAction};
     use WitnessSubscriberChecks::{Active, Interested, Notify};
 
     impl Default for WitnessSubscriber {
         fn default() -> Self {
             WitnessSubscriber {
-                operations: Rc::new(RefCell::new(vec![])),
+                name: "",
+                operations: Arc::new(RwLock::new(vec![])),
                 should_be_interested: true,
                 should_be_active: true,
                 should_notify_error: false,
@@ -297,11 +311,11 @@ mod tests {
     }
 
     impl Subscriber for WitnessSubscriber {
-        type State = BasicState;
-        type Flag = BasicFlag;
+        type State = WitnessState;
+        type Flag = TestFlag;
 
         fn notify(&mut self, new_state: &Self::State) -> Result<(), SubscriberError> {
-            self.operations.borrow_mut().push(Notify(new_state.clone()));
+            self.operations.write().unwrap().push(Notify(new_state.clone()));
             if self.should_notify_error {
                 Err(SubscriberError::MissingState)
             } else {
@@ -310,21 +324,22 @@ mod tests {
         }
 
         fn is_active(&self) -> bool {
-            self.operations.borrow_mut().push(Active);
+            self.operations.write().unwrap().push(Active);
             self.should_be_active
         }
 
         fn interested_in(&self, offered: &EnumSet<Self::Flag>) -> bool {
             self.operations
-                .borrow_mut()
+                .write()
+                .unwrap()
                 .push(Interested(offered.clone()));
             self.should_be_interested
         }
     }
 
     type MessagingPair = (
-        MessageSender<Message<FakeAction, BasicRuntimeAction>>,
-        Receiver<Message<FakeAction, BasicRuntimeAction>>,
+        MessageSender<Message<TestAction, TestRuntimeAction>>,
+        Receiver<Message<TestAction, TestRuntimeAction>>,
     );
     #[fixture]
     fn sender_receiver() -> MessagingPair {
@@ -332,15 +347,15 @@ mod tests {
         (MessageSender::new(sender), receiver)
     }
 
-    fn witness_reducer(state: &mut BasicState, action: FakeAction) -> ActionProducts<MClient> {
+    fn witness_reducer(state: &mut WitnessState, action: TestAction) -> ActionProducts<TestClient> {
         state.called.push(action);
         ActionProducts::none()
     }
 
     fn flags_producing_reducer(
-        state: &mut BasicState,
-        action: FakeAction,
-    ) -> ActionProducts<MClient> {
+        state: &mut WitnessState,
+        action: TestAction,
+    ) -> ActionProducts<TestClient> {
         state.called.push(action.clone());
         let (cmds, flags) = match action {
             CmdGeneratingAction(_, CmdProduct::Direct(actions)) => {
@@ -353,10 +368,11 @@ mod tests {
     }
 
     fn cmd_producing_reducer(
-        state: &mut BasicState,
-        action: FakeAction,
-    ) -> ActionProducts<MClient> {
+        state: &mut WitnessState,
+        action: TestAction,
+    ) -> ActionProducts<TestClient> {
         state.called.push(action.clone());
+
         let cmds = match action {
             BasicAction(_) => vec![],
             CmdGeneratingAction(_, cmd_product) => match cmd_product {
@@ -377,9 +393,9 @@ mod tests {
     }
 
     fn all_dirty_flags_reducer(
-        state: &mut BasicState,
-        action: FakeAction,
-    ) -> ActionProducts<MClient> {
+        state: &mut WitnessState,
+        action: TestAction,
+    ) -> ActionProducts<TestClient> {
         state.called.push(action);
         ActionProducts {
             cmds: vec![],
@@ -387,21 +403,36 @@ mod tests {
         }
     }
 
-    fn empty_runtime_reducer(_action: BasicRuntimeAction) -> RuntimeProducts<MClient> {
+    fn empty_runtime_reducer(_action: TestRuntimeAction) -> RuntimeProducts<TestClient> {
         RuntimeProducts::none()
     }
 
+    fn products_producing_runtime_reducer(action: TestRuntimeAction) -> RuntimeProducts<TestClient> {
+        match action {
+            TestRuntimeAction::CreateSubscriber(subscriber) => RuntimeProducts {
+                subscriber: Some(Box::new(subscriber)),
+                actions: vec![],
+            },
+
+            TestRuntimeAction::CreateActions(actions) => RuntimeProducts {
+                subscriber: None,
+                actions,
+            }
+        }
+    }
+
     #[fixture]
-    fn state() -> BasicState {
-        BasicState { called: vec![] }
+    fn state() -> WitnessState {
+        WitnessState { called: vec![] }
     }
 
     #[fixture]
     fn config(
-        #[default(witness_reducer)] reducers: Reducer<MClient>,
+        #[default(witness_reducer)] reducer: Reducer<TestClient>,
+        #[default(empty_runtime_reducer)] runtime_reducer: RuntimeReducer<TestClient>,
         sender_receiver: MessagingPair,
-        state: BasicState,
-    ) -> RuntimeConfig<MClient, BasicJobDispatcher> {
+        state: WitnessState,
+    ) -> RuntimeConfig<TestClient, WitnessJobDispatcher> {
         let (messages_tx, messages_rx) = sender_receiver;
         RuntimeConfig {
             services: WitnessServices {
@@ -409,9 +440,9 @@ mod tests {
             },
             state,
             middlewares: vec![],
-            reducer: reducers,
-            runtime_reducer: empty_runtime_reducer,
-            jobs_dispatcher: BasicJobDispatcher {
+            reducer,
+            runtime_reducer,
+            jobs_dispatcher: WitnessJobDispatcher {
                 called: Rc::new(Cell::new(0)),
             },
             messages_rx,
@@ -421,7 +452,7 @@ mod tests {
 
     #[rstest]
     fn received_action_should_call_middlewares_and_reducer(
-        mut config: RuntimeConfig<MClient, BasicJobDispatcher>,
+        mut config: RuntimeConfig<TestClient, WitnessJobDispatcher>,
     ) {
         let witness = Rc::new(RefCell::new(vec![]));
         config.middlewares.push(Box::new(WitnessMiddleware {
@@ -429,15 +460,15 @@ mod tests {
         }));
 
         let mut runtime = Runtime::new(config);
-        runtime.process_message(Message::Action(FakeAction::BasicAction("basic")));
+        runtime.process_message(Action(BasicAction("basic")));
 
-        assert_eq!(*witness.borrow(), vec![FakeAction::BasicAction("basic")]);
-        assert_eq!(runtime.state.called, vec![FakeAction::BasicAction("basic")]);
+        assert_eq!(*witness.borrow(), vec![BasicAction("basic")]);
+        assert_eq!(runtime.state.called, vec![BasicAction("basic")]);
     }
 
     #[rstest]
     fn direct_command_side_fx_should_execute_product_action(
-        #[with(cmd_producing_reducer)] config: RuntimeConfig<MClient, BasicJobDispatcher>,
+        #[with(cmd_producing_reducer)] config: RuntimeConfig<TestClient, WitnessJobDispatcher>,
     ) {
         let mut runtime = Runtime::new(config);
         // An action that will generate a direct command with a pair of (BasicAction) actions as product.
@@ -446,7 +477,7 @@ mod tests {
             CmdProduct::Direct(vec![BasicAction("basic2"), BasicAction("basic3")]),
         );
         // An action that will generate a direct command with a pair of actions as product: a basic action and the sub-action above
-        let message = Message::Action(CmdGeneratingAction(
+        let message = Action(CmdGeneratingAction(
             "initial",
             CmdProduct::Direct(vec![BasicAction("basic1"), sub_action]),
         ));
@@ -462,7 +493,7 @@ mod tests {
 
     #[rstest]
     fn queue_command_side_fx_should_not_execute_product_action_but_send_it(
-        #[with(cmd_producing_reducer)] config: RuntimeConfig<MClient, BasicJobDispatcher>,
+        #[with(cmd_producing_reducer)] config: RuntimeConfig<TestClient, WitnessJobDispatcher>,
     ) {
         let mut runtime = Runtime::new(config);
         // An action that will generate a queued command with a pair of (BasicAction) actions as product.
@@ -471,7 +502,7 @@ mod tests {
             CmdProduct::Queue(vec![BasicAction("basic2"), BasicAction("basic3")]),
         );
         // An action that will generate a queued command with a pair of actions as product: a basic action and the sub-action above
-        let message = Message::Action(CmdGeneratingAction(
+        let message = Action(CmdGeneratingAction(
             "initial",
             CmdProduct::Queue(vec![BasicAction("basic1"), sub_action]),
         ));
@@ -485,14 +516,14 @@ mod tests {
                 .messages_rx
                 .try_recv()
                 .expect("Should not fail, next message available"),
-            Message::Action(BasicAction("basic1"))
+            Action(BasicAction("basic1"))
         );
         assert_matches!(
             runtime
                 .messages_rx
                 .try_recv()
                 .expect("Should not fail, next message available"),
-            Message::Action(CmdGeneratingAction("second", _))
+            Action(CmdGeneratingAction("second", _))
         );
         assert_eq!(
             runtime
@@ -505,11 +536,11 @@ mod tests {
 
     #[rstest]
     fn async_cmd_should_execute_task_send_result(
-        #[with(cmd_producing_reducer)] config: RuntimeConfig<MClient, BasicJobDispatcher>,
+        #[with(cmd_producing_reducer)] config: RuntimeConfig<TestClient, WitnessJobDispatcher>,
     ) {
         let job_witness = config.jobs_dispatcher.called.clone();
         let mut runtime = Runtime::new(config);
-        let message = Message::Action(CmdGeneratingAction(
+        let message = Action(CmdGeneratingAction(
             "async",
             CmdProduct::Async(Box::new(BasicAction("async_result"))),
         ));
@@ -525,7 +556,7 @@ mod tests {
                 .messages_rx
                 .try_recv()
                 .expect("Should not fail, next message available"),
-            Message::Action(BasicAction("async_result"))
+            Action(BasicAction("async_result"))
         );
         assert_eq!(
             runtime
@@ -538,13 +569,13 @@ mod tests {
 
     #[rstest]
     fn env_cmd_should_execute_on_environment_send_result(
-        #[with(cmd_producing_reducer)] config: RuntimeConfig<MClient, BasicJobDispatcher>,
+        #[with(cmd_producing_reducer)] config: RuntimeConfig<TestClient, WitnessJobDispatcher>,
     ) {
         let services_witness = config.services.called.clone();
         let mut runtime = Runtime::new(config);
-        let message = Message::Action(CmdGeneratingAction(
+        let message = Action(CmdGeneratingAction(
             "services",
-            CmdProduct::Env(FakeServiceCommand),
+            CmdProduct::Env(TestServiceCommand),
         ));
 
         runtime.process_message(message);
@@ -565,14 +596,14 @@ mod tests {
 
     #[rstest]
     fn recursive_action_execution_should_accumulate_flags(
-        #[with(flags_producing_reducer)] config: RuntimeConfig<MClient, BasicJobDispatcher>,
+        #[with(flags_producing_reducer)] config: RuntimeConfig<TestClient, WitnessJobDispatcher>,
     ) {
         let subscriber = WitnessSubscriber::default();
         let op_witness = subscriber.operations.clone();
         let mut runtime = Runtime::new(config);
         runtime.subscribers.push(Box::new(subscriber));
 
-        let action: Vec<FakeAction> = EnumSet::<BasicFlag>::all()
+        let action: Vec<TestAction> = EnumSet::<TestFlag>::all()
             .into_iter()
             .map(FlagAction)
             .rev()
@@ -589,7 +620,8 @@ mod tests {
         )));
 
         let flag_ops: Vec<_> = op_witness
-            .borrow()
+            .read()
+            .unwrap()
             .iter()
             .cloned()
             .filter_map(|op| {
@@ -600,23 +632,48 @@ mod tests {
             })
             .collect();
 
-        assert_eq!(flag_ops, vec![EnumSet::<BasicFlag>::all()]);
+        assert_eq!(flag_ops, vec![EnumSet::<TestFlag>::all()]);
     }
 
-    // todo: cmds can also generate actions
-    // todo: test runtime actions
+    #[rstest]
+    fn runtime_action_subscriber_product_should_add_subscriber_subscriber_is_called(
+        #[with(witness_reducer, products_producing_runtime_reducer)] config: RuntimeConfig<TestClient, WitnessJobDispatcher>,
+    ) {
+        let subscriber = WitnessSubscriber::default();
+        let witness_op = subscriber.operations.clone();
+
+        let mut runtime = Runtime::new(config);
+        runtime.process_message(Message::Runtime(TestRuntimeAction::CreateSubscriber(subscriber)));
+
+        assert_eq!(runtime.subscribers.len(), 1);
+        assert_eq!(*witness_op.read().unwrap(), vec![Active, Interested(EnumSet::empty()), Notify(WitnessState { called: vec![] })]);
+    }
+
+    #[rstest]
+    fn runtime_action_action_product_should_add_action_and_execute_it_immediately(
+        #[with(cmd_producing_reducer, products_producing_runtime_reducer)] config: RuntimeConfig<TestClient, WitnessJobDispatcher>,
+    ) {
+        let mut runtime = Runtime::new(config);
+        let indirect_action = CmdGeneratingAction("indirect", CmdProduct::Direct(vec![BasicAction("3")]));
+        let resulting_actions = vec![BasicAction("1"), BasicAction("2"), indirect_action];
+        runtime.process_message(Message::Runtime(TestRuntimeAction::CreateActions(resulting_actions.clone())));
+
+        let mut executed_actions = resulting_actions;
+        executed_actions.push(BasicAction("3"));
+        assert_eq!(runtime.state.called, executed_actions);
+    }
 
     #[rstest]
     #[case::active_and_interested(
         true,
         true,
-        vec![Active, Interested(EnumSet::<BasicFlag>::all()), Notify(BasicState { called: vec![BasicAction("basic")] })]
+        vec![Active, Interested(EnumSet::<TestFlag>::all()), Notify(WitnessState { called: vec![BasicAction("basic")] })]
     )]
     #[case::inactive(false, true, vec![Active])]
-    #[case::not_interested(true, false, vec![Active, Interested(EnumSet::<BasicFlag>::all())])]
+    #[case::not_interested(true, false, vec![Active, Interested(EnumSet::<TestFlag>::all())])]
     #[case::inactive_and_not_interested(false, false, vec![Active])]
     fn received_action_generating_products_should_only_call_active_interested_subscribers(
-        #[with(all_dirty_flags_reducer)] config: RuntimeConfig<MClient, BasicJobDispatcher>,
+        #[with(all_dirty_flags_reducer)] config: RuntimeConfig<TestClient, WitnessJobDispatcher>,
         #[case] active: bool,
         #[case] interested: bool,
         #[case] expected_checks: Vec<WitnessSubscriberChecks>,
@@ -630,8 +687,8 @@ mod tests {
         let mut runtime = Runtime::new(config);
         runtime.subscribers.push(Box::new(subscriber));
 
-        runtime.process_message(Message::Action(FakeAction::BasicAction("basic")));
+        runtime.process_message(Action(BasicAction("basic")));
 
-        assert_eq!(*op_witness.borrow(), expected_checks);
+        assert_eq!(*op_witness.read().unwrap(), expected_checks);
     }
 }
